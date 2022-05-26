84cdaac3f9ec7345f1af12bb3456cb6ea21899ca
Oracy Martos <oracy.martos@vaees.com>
2022-05-26 01:16:48 -0300

    refactor: simplify code

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { badRequest } from '../helpers/http-helper';
 
 export class SignUpController {
     handle(httpRequest: HttpRequest): HttpRespose {
-        if (!httpRequest.body.name) {
-            return badRequest(new MissingPramError('name'));
-        }
-        if (!httpRequest.body.email) {
-            return badRequest(new MissingPramError('email'));
+        const requiredFields = ['name', 'email'];
+        for (const field of requiredFields) {
+            if (!httpRequest.body[field]) {
+                return badRequest(new MissingPramError(field));
+            }
         }
     }
 }

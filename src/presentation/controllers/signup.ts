70ab9be8467af0212b43395c26ca005ca2fda7d8
Oracy Martos <oracy.martos@vaees.com>
2022-05-26 12:03:38 -0300

    feat: ensure SignUpController calls AddAccount with correct values

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,12 +1,17 @@
 import { badRequest, serverError } from '../helpers/http-helper';
 import { InvalidParamError, MissingParamError } from '../errors';
 import { Controller, EmailValidator, HttpRequest, HttpRespose } from '../protocols';
+import { AddAccount } from '@/domain/usecases/add-account';
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator;
-    constructor(emailValidator: EmailValidator) {
+    private readonly addAccount: AddAccount;
+
+    constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
         this.emailValidator = emailValidator;
+        this.addAccount = addAccount;
     }
+
     handle(httpRequest: HttpRequest): HttpRespose {
         try {
             const requiredFields = ['name', 'email', 'password', 'passwordConfirmation'];
@@ -15,7 +20,7 @@ export class SignUpController implements Controller {
                     return badRequest(new MissingParamError(field));
                 }
             }
-            const { email, password, passwordConfirmation } = httpRequest.body
+            const { name, email, password, passwordConfirmation } = httpRequest.body;
             if (httpRequest.body.password !== passwordConfirmation) {
                 return badRequest(new InvalidParamError('passwordConfirmation'));
             }
@@ -23,6 +28,11 @@ export class SignUpController implements Controller {
             if (!isValid) {
                 return badRequest(new InvalidParamError('email'));
             }
+            this.addAccount.add({
+                name,
+                email,
+                password,
+            });
         } catch (error) {
             return serverError();
         }

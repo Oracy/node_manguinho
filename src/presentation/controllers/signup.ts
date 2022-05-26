4cf94d2c82251ac9ebb02bb85ed3f7b836de23fb
Oracy Martos <oracy.martos@vaees.com>
2022-05-26 02:23:41 -0300

    refactor: move server error to a helper method

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,7 +4,6 @@ import { badRequest, serverError } from '../helpers/http-helper';
 import { Controller } from '../protocols/controller';
 import { EmailValidator } from '../protocols/email-validator';
 import { InvalidParamError } from '../errors/invalid-param-error';
-import { ServerError } from '../errors/server-param-error';
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator;

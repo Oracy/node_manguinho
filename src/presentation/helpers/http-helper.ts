f2f1890e91a0914f20eccdb58c7b116d545554b5
Oracy Martos <oracy.martos@vaees.com>
2022-05-26 02:23:26 -0300

    refactor: move server error to a helper method

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,6 +1,12 @@
+import { ServerError } from '../errors/server-param-error';
 import { HttpRespose } from '../protocols/http';
 
 export const badRequest = (error: Error): HttpRespose => ({
     statusCode: 400,
     body: error,
+});
+
+export const serverError = (): HttpRespose => ({
+    statusCode: 500,
+    body: new ServerError(),
 });
